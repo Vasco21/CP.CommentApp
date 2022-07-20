4355ab43077aa80a7277b0eddce69bec45d8d085
Vasco21 <vascoeti123@gmail.com>
2022-07-20 04:40:27 +0200

    update

---
+++ build/precache-manifest.e139d7df6e01b1dfb9b773415ff99528.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc86d4570f39761aa1f835b17a38852c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e05b892903d334ada178",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "f7806c40ce6cf06493b7",
+    "url": "/static/js/2.37aa7377.chunk.js"
+  },
+  {
+    "revision": "168125c8a08d6f1a37c2fa00bdad3790",
+    "url": "/static/js/2.37aa7377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e05b892903d334ada178",
+    "url": "/static/js/main.303d1118.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.303d1118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "c3eee0e804238f32da76925e5a4d9698",
+    "url": "/static/media/memoriesText.c3eee0e8.png"
+  }
+]);
